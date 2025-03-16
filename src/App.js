4cc9abb515cf88e8d722d872c63e3eeab65dfba4
user01 <febsnight@gmail.com>
2025-03-16 18:00:09 +0900

    section component frame

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Today from './pages/Today'
-import Developer from './pages/Developer'
-import Webd from './pages/Webd'
-import Website from './pages/Website'
-import Gaps from './pages/Gaps'
-import Port from './pages/Port'
-import Youtube from './pages/Youtube'
-import Channel from './pages/Channel'
-import Video from './pages/Video'
-import Search from './pages/Search'
-import Not from './pages/Not'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Today from "./pages/Today";
+import Developer from "./pages/Developer";
+import Webd from "./pages/Webd";
+import Website from "./pages/Website";
+import Gaps from "./pages/Gaps";
+import Port from "./pages/Port";
+import Youtube from "./pages/Youtube";
+import Channel from "./pages/Channel";
+import Video from "./pages/Video";
+import Search from "./pages/Search";
+import Not from "./pages/Not";
+import Header from "./components/section/Header";
+import Main from "./components/section/Main";
+import Footer from "./components/section/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/today' element={<Today/>}/>
-      <Route path='/developer' element={<Developer/>}/>
-      <Route path='/webd' element={<Webd/>}/>
-      <Route path='/website' element={<Website/>}/>
-      <Route path='/gsap' element={<Gaps/>}/>
-      <Route path='/port' element={<Port/>}/>
-      <Route path='/youtube' element={<Youtube/>}/>
-      <Route path='/channel/:channelID' element={<Channel/>}/>
-      <Route path='/video/:videoID' element={<Video/>}/>
-      <Route path='/search/:searchID' element={<Search/>}/>
-      <Route path='/*' element={<Not/>}/>
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/webd" element={<Webd />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/gsap" element={<Gaps />} />
+          <Route path="/port" element={<Port />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel/:channelID" element={<Channel />} />
+          <Route path="/video/:videoID" element={<Video />} />
+          <Route path="/search/:searchID" element={<Search />} />
+          <Route path="/*" element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/section/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <main id='main' role='main'>
+        main
+    </main>
+  )
+}
+
+export default Main
