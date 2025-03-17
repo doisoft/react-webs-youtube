4fdc6ec73008daef80f5090aa3a7156c4f3acaca
user01 <febsnight@gmail.com>
2025-03-17 10:06:48 +0900

    changed header data

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <h1 className="header__logo">
+      <a href="/">
+        <em aria-hidden="true"></em>
+        <span>
+          webs <br />
+          Youtube
+        </span>
+      </a>
+    </h1>
+  );
+};
---
+++ src/components/header/Sns.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { snsLink } from "../../data/header";
+
+export const Sns = () => {
+  return (
+    <div className="header__sns">
+      <ul>
+        {snsLink.map((sns, key) => {
+          return (
+            <li key={key}>
+              <a
+                href={sns.url}
+                target="_blankl"
+                rel="noopener noreferrer"
+                aria-label={sns.title}
+              >
+                <span>{sns.icon}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
