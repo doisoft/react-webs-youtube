be1e2cbfe2d777456327c81c59f52e171b01e109
user01 <febsnight@gmail.com>
2025-03-18 16:00:34 +0900

    Developer Recommendation Page

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import { developerText } from '../../data/developer'
+import { Link } from 'react-router-dom'
+
 const Developer = () => {
-  return (
-    <div>Developer</div>
-  )
+    return (
+        <section id='developer'>
+            <h2>🤗추천 개발자를 소개합니다.</h2>
+            <div className='developer__inner overflow'>
+                {developerText.map((dev, key) => {
+                    return (
+                        <div className='developer' key={key}>
+                            <div className='developer__img play__icon'>
+                                <Link to={`/channel/${dev.channelAddress}`}>
+                                    <img src={dev.img} alt={dev.name} />
+                                </Link>
+                            </div>
+                            <div className='developer__info'>
+                                <Link to={`/channel/${dev.channelAddress}`}>
+                                    {dev.name}
+                                </Link>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
+    )
 }
 
 export default Developer
--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { developerText } from '../data/developer'
+import { Link } from 'react-router-dom'
 
 const Developer = () => {
-  return (
-    <Main  title="추천 개발자" description="추천 개발자 페이지입니다.">
-      Developer
-    </Main>
-  )
+    return (
+        <Main title='추천 개발자' description='추천 개발자 페이지입니다.'>
+            <section id='developerPage'>
+                <div className='developer__inner'>
+                    {developerText.map((dev, key) => {
+                        return (
+                            <div className='developer' key={key}>
+                                <div className='developer__img play__icon'>
+                                    <Link to={`/channel/${dev.channelAddress}`}>
+                                        <img src={dev.img} alt={dev.name} />
+                                    </Link>
+                                </div>
+                                <div className='developer__info'>
+                                    <Link to={`/channel/${dev.channelAddress}`}>
+                                        {dev.name}
+                                    </Link>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
+        </Main>
+    )
 }
 
 export default Developer
